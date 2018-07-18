823a39047322fbfe21055ac500b8dedba36655c5
Eric Fithian <ef2413@columbia.edu>
2018-07-18 16:40:06 -0400

    Refactored this to make it into a multistep todo app

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,53 @@
 import React, {Component} from 'react';
 
-export default class App extends Component {
-    render () {
-        <p>This is my work</p>
+export default class ProjectForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      title: '',
+      subject: '',
+      description: ''
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'text' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input 
+          name="name"
+          type="text"
+          onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Title:
+          <input type="text" title="title" />
+        </label>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+      </form>
+    );
+  }
 }
--- store.js
+++ store.js
@@ -1,10 +1,9 @@
 
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import messageReducer from './reducers/message'
 import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
-  messageReducer
+  
 })
 const store = createStore(
   reducer,
